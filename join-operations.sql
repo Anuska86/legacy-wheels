2234803a9f5b8807d54b420a61bac2bbae634e44
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-05 12:04:08 +0100

    Create join-operations.sql

---
+++ join-operations.sql
@@ -0,0 +1,27 @@
+/*
+	Select the brand, model, price, sold, sold_price columns
+		from sold_cars
+	Left join with cars
+		matching sold_cars.cars_id to cars.id
+*/
+
+SELECT brand, model, price, sold, sold_price 
+  FROM sold_cars SC
+  LEFT JOIN cars C ON sold_cars.cars_id = cars.id;
+
+  
+SELECT brand, model, price, sold, sold_price 
+  FROM sold_cars SC
+  LEFT JOIN cars C ON SC.cars_id = C.id;  
+
+  /*
+	Select name, role, city, state
+		From the staff table
+	Left join with the dealerships table where the dealership_id in staff
+			matches with the id in dealerships
+*/
+
+
+SELECT name, role, city, state
+FROM staff S
+LEFT JOIN dealerships D ON D.id=S.dealership_id; 
